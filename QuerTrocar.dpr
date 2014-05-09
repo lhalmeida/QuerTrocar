d183429f5ef054fd42b7c17028cf366a442e7630
ApoloBR <saulo.calebe@outlook.com>
2014-05-09 03:02:59 -0300

    Inicial

---
+++ QuerTrocar.dpr
@@ -0,0 +1,15 @@
+program QuerTrocar;
+
+uses
+  System.StartUpCopy,
+  FMX.MobilePreview,
+  FMX.Forms,
+  uFormMain in 'uFormMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
