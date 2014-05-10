9fcd58c8171795428aaff31070d22e92cc721539
Lucas Henrique <lh.almeida@outlook.com>
2014-05-10 00:03:38 -0300

    Commit 1

---
+++ QuerTrocar.pas
@@ -0,0 +1,29 @@
+unit QuerTrocar;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  System.Math.Vectors, FMX.Controls3D, FMX.Layers3D, FMX.Objects, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Text1: TText;
+    TextLayer3D1: TTextLayer3D;
+    Text2: TText;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
